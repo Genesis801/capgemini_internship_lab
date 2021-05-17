8bee41f36f455c352d1301b21f2e1a5ecafb1457
Genesis801 <debajyotiroy21@gmail.com>
2021-05-17 12:41:35 +0530

    Bug Fixed in Exercise 9 code/ User Input Added/Small bug fixes

--- Lab3/CountDown.java
+++ Lab3/CountDown.java
@@ -1,14 +1,25 @@
+package lab_3;
+
+import java.util.*;
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class CountDown {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		LocalDate pdate=LocalDate.of(2021,05,18);
+		Scanner sc = new Scanner(System.in);
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		System.out.print("Enter the date in format: (dd/mm/yyyy): ");
+		String date = sc.nextLine();
+		LocalDate setDate = LocalDate.parse(date, df);
+		
 		LocalDate now=LocalDate.now();
 		
-		Period diff=Period.between(pdate, now);
-		System.out.println("Reamaining "+diff.getYears()+" years||"+diff.getMonths()+" Months||"+diff.getDays()+"days");
+		Period diff=Period.between(now,setDate);
+		System.out.println("Reamaining "+diff.getYears()+" years||"+diff.getMonths()+" Months||"+diff.getDays()+" days");
+		
+		sc.close();
 	}
 }
