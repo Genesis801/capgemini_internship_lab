00376e0e25ad7086d97012de72562949faca7dd1
Debajyoti Roy <44739430+Genesis801@users.noreply.github.com>
2021-05-19 09:56:40 +0530

    Create Employee.java

---
+++ Lab4/Exercise2/com.cg.eis.bean/Employee.java
@@ -0,0 +1,10 @@
+package com.cg.eis.bean;
+public class Employee {
+	
+	public int id;
+	public String name;
+	public double salary;
+	public String designation;
+	public String insuranceScheme;
+	
+}
