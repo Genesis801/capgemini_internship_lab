66168dd3c0d01f9efb7ff7d9e31208e16f324f64
Debajyoti Roy <44739430+Genesis801@users.noreply.github.com>
2021-05-21 18:32:39 +0530

    Create MainEmployee.java

---
+++ Lab5/Exercise3/MainEmployee.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+	
+		Scanner sc=new Scanner(System.in);
+		
+		try {
+			System.out.print("Enter the Employee ID : ");
+			int id=sc.nextInt();
+			
+      System.out.print("Enter the Employee Name : ");
+			sc.nextLine();
+      String name=sc.nextLine();
+			
+      System.out.print("Enter Employee Age : ");
+			int age=sc.nextInt();
+			
+      System.out.print("Enter Employee salary : ");
+			double salary=sc.nextDouble();
+			
+      Employee e = new Employee(id,name,age,salary);
+			e.validateSalary(salary);
+		
+    }catch(EmployeeException e) {
+			System.out.println("Error : "+e);
+		}
+		sc.close();
+	}
+
+}
