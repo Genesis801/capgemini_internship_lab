ea40c30d74253058735bfc779bed71a147cbb144
Debajyoti Roy <44739430+Genesis801@users.noreply.github.com>
2021-06-07 08:57:58 +0530

    Create StringFormatUsingLambdaExpression.java

---
+++ Lab9/StringFormatUsingLambdaExpression.java
@@ -0,0 +1,8 @@
+package com.capgemini.lab9;
+
+@FunctionalInterface
+public interface StringFormatUsingLambdaExpression {
+	
+	// method to insert a space between each characters of a given string
+	String returnExpression(String string);
+}
