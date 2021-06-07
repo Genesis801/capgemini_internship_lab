c6019c1766eb3e23bd8a689f10154bdfc84ea4f9
Debajyoti Roy <44739430+Genesis801@users.noreply.github.com>
2021-06-07 08:53:54 +0530

    Create UsernamePasswordValidationUsingLambdaExpression.java

---
+++ Lab9/UsernamePasswordValidationUsingLambdaExpression.java
@@ -0,0 +1,8 @@
+package com.capgemini.lab9;
+
+@FunctionalInterface
+public interface UsernamePasswordValidationUsingLambdaExpression {
+
+	//boolean method to validate username & password
+	boolean validateusername_password(String username,String password);
+}
