842aca46a7e4509997f54feb2b085b82e52be25d
Debajyoti Roy <44739430+Genesis801@users.noreply.github.com>
2021-06-07 09:02:12 +0530

    Create ExponentsUsingLambdaExpression.java

---
+++ Lab9/ExponentsUsingLambdaExpression.java
@@ -0,0 +1,9 @@
+package com.capgemini.lab9;
+
+@FunctionalInterface
+public interface ExponentsUsingLambdaExpression {
+
+	// method to calculate the value of x raised to the power of y
+	int power(int x,int y);
+	
+}
